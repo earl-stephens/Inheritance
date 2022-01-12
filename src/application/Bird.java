1b2ea5394df6fe6521c8ba793a705b2028850da1
earl-stephens <stephensearl@hotmail.com>
2022-01-11 22:42:14 -0700

    Demonstrate basic inheritance

---
+++ src/application/Bird.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Bird extends Animal {
+	//Bird is an Animal, and Bird can eat
+	//extends Animal means that Bird has access to 
+	//the Animal methods
+	//Bird is a subclass of Animal
+	//Animal is the super class
+	
+	public void layEgg() {
+		System.out.println("Laying an egg...");
+	}
+}
